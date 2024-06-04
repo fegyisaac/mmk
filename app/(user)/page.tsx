9267b9a8aaa9adf191824545e1f437f230237021
zicojr <fegyisaac160@gmail.com>
2024-06-04 16:28:17 +0100

    commit

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,32 @@
-import Banner from '@/components/Banner'
-import React from 'react'
+import Banner from "@/components/Banner";
+import React from "react";
+import { groq } from "next-sanity";
+import { client } from "@/lib/sanityClient";
 
-const Homepage = () => {
+const bannerQuery = groq`*[_type == 'banner']{
+  image,
+  _id
+} | order(_createdAt asc)`;
+
+// const newArrivalQuery = groq`*[_type == 'product' && position == 'New Arrivals']{
+// ...
+// } | order(_createdAt asc)`;
+
+// const bestSellersQuery = groq`*[_type == 'product' && position == 'Bestsellers']{
+//   ...
+//  } | order(_createdAt asc)`;
+// const specialOffersQuery = groq`*[_type == 'product' && position == 'Special Offers']{
+//   ...
+//  } | order(_createdAt asc)`;
+
+const Homepage = async () => {
+  const banners = await client.fetch(bannerQuery);
+
   return (
-    <div>
-      <Banner />
-    </div>
-  )
-}
+    <main className="text-sm overflow-hidden min-h-screen">
+      <Banner banners={banners} />
+    </main>
+  );
+};
 
 export default Homepage;
